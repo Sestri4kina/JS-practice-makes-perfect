d0772f40e68aab1acec400104ba06bc339619e97
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-19 12:42:25 +0200

    Add codewars's vowel count task

---
+++ js/codewars-vowel-count.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Sestri4kina on 18.12.2016.
+ *
+ * Return the number (count) of vowels in the given string.
+ */
+
+function getCount(str) {
+    var newStr = str.replace(/[aeiou]/g, 0).split('');
+    return (newStr.filter(x => x==='0')).length;
+}
+
+var result = getCount("abracadabra");
+console.log(`Should return: 5. Output is: ${result}`);
+
+var result1 = getCount('pear tree');
+console.log(`Should return: 4. Output is: ${result1}`);
