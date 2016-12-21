be02d27d505f365e0528a752aece2f7e42f47d60
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-21 18:02:03 +0200

    Add changes to 'shortest word' task for testing purposes

--- src/js/codewars-shortest-word.js
+++ src/js/codewars-shortest-word.js
@@ -5,17 +5,16 @@
  * String will never be empty and you do not need to account for different data types.
  */
 
-function findShortest(data) {
-    //var newData = data.split(' ');
+module.exports = function findShortest(data) {
     return (data.split(' ').map( x => x.length ).sort( (a,b) => a-b ))[0];
 }
-
+/*
 var result = findShortest("bitcoin take over the world maybe who knows perhaps");
 console.log(`Should return: 3. Output is: ${result}`);
 
 var result1 = findShortest("turns out random test cases are easier than writing out basic ones");
 console.log(`Should return: 3. Output is: ${result1}`);
-
-/*
+*
+*
 That's the first time that I find my solution the most elegant amongst all the others.
  */
