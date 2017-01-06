371fca0bbfc2b3ea156fb146141e8a908658155c
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-06 19:44:39 +0200

    Refactor solution of the 'even the last' task

--- src/js/checkio-even-the-last.js
+++ src/js/checkio-even-the-last.js
@@ -14,19 +14,25 @@
  *
  */
 
-function evenLast(arr){
-    if (arr.length === 0) {
-        return 0;
-    } else {
-        var sum = 0;
-        for(var i = 0;  i < arr.length ; i++){
-            if (i % 2 === 0 ) {
-                sum = sum + arr[i];
-            }
-        }
-        return sum * arr[arr.length - 1];
-    }
-}
+module.exports = function evenLast(arr){
+    return arr.length === 0 ? 0 :
+        (arr[arr.length - 1] * arr.reduce((prev, curr, index) => {return index % 2 === 0 ? prev + curr : prev}));
+};
+
+/*
+previous solution:
+ if (arr.length === 0) {
+ return 0;
+ } else {
+ var sum = 0;
+ for(var i = 0;  i < arr.length ; i++){
+ if (i % 2 === 0 ) {
+ sum = sum + arr[i];
+ }
+ }
+ return sum * arr[arr.length - 1];
+ }
+
 
 var result = evenLast([0, 1, 2, 3, 4, 5]);
 console.log(`Should return: 30. Output is: ${result}`);
@@ -36,3 +42,4 @@ console.log(`Should return: 0. Output is: ${result1}`);
 
 var result2 = evenLast([0, 2, 4, 7]);
 console.log(`Should return: 28. Output is: ${result2}`);
+*/
