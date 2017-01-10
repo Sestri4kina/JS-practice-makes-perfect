56e6abbd6f4a722d48b9e62fe99b17c5ff4912f5
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-10 23:03:17 +0200

    Add changes to 'maxed out' to apply test

--- src/js/codewars-maxed-out.js
+++ src/js/codewars-maxed-out.js
@@ -7,14 +7,14 @@
  *  return the sum otherwise return "You've pushed me to the max!".
  *  You can assume that you will receive a valid array of numbers.
  */
-function maxedOut(data){
+module.exports = function maxedOut(data){
     var result = data.map((x) => { return Math.pow(x, 3)})
         .reduce((prev, curr) => {return prev + curr});
     return (result >= Number.MIN_SAFE_INTEGER) && (result <= Number.MAX_SAFE_INTEGER) ?
         result :  "You've pushed me to the max!";
 
-}
-
+};
+/*
 var result = maxedOut([1,2]);
 console.log(`Should return : 9. Output is: ${result}`);
 
@@ -23,3 +23,4 @@ console.log(`Should return : "You've pushed me to the max!". Output is: ${result
 
 var result2 = maxedOut([-530,230,5600,40,5,7200]);
 console.log(`Should return : 548727354125. Output is: ${result2}`);
+    */
