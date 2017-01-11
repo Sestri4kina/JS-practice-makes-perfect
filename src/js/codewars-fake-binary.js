6f4f7f2068cceaec262e7fe77eedb86d761db1d1
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-11 22:27:42 +0200

    Add changes to the 'fake binary' to apply test

--- src/js/codewars-fake-binary.js
+++ src/js/codewars-fake-binary.js
@@ -5,12 +5,14 @@
  * you should replace any digit below 5 with '0' and any digit 5 and above with '1'.
  * Return the resulting string.
  */
-function fakeBin(str) {
+module.exports = function fakeBin(str) {
     return str.split('').map(x => x < 5 ? 0 : 1).join('');
-}
+};
 
+/*
 var result = fakeBin('45385593107843568');
 console.log(`Should return: '01011110001100111'. Output is: ${result}`);
 
 var result1 = fakeBin('509321967506747');
 console.log(`Should return: '101000111101101'. Output is: ${result1}`);
+    */
