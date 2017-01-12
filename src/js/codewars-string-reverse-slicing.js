f733b7cc00a4121f012efc3fa03dc720d8b73222
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-12 23:33:13 +0200

    Add solution for the 'string reverse slicing' task

---
+++ src/js/codewars-string-reverse-slicing.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Sestri4kina on 12.01.2017.
+ *
+ * You'll be given a string of characters as an input.
+ * Create a function reverse_slice/reverseSlice that returns a list of strings:
+ * (a) in the reverse order of the original string, and
+ * (b) with each successive string starting one character further in from the end of the original string.
+ */
+function reverseSlice(str){
+    var data = str.split('').reverse().join(''),
+        arr = [];
+    for(var i = 0; i < str.length; i++){
+        arr.push(data.slice(i, str.length));
+    }
+    return arr;
+}
+
+var result = reverseSlice('123');
+console.log(`Should return: ['321', '21', '1']. Output is: ${result}`);
+
+var result1 = reverseSlice('abcde');
+console.log(`Should return:  ['edcba', 'dcba', 'cba', 'ba', 'a']. Output is: ${result1}`);
+
