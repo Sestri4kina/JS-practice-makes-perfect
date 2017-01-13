6eaeb6c40a591c772c78f78ee577e2321729f2d4
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-13 16:23:46 +0200

    Add changes to the 'string reverse slicing' to apply test

--- src/js/codewars-string-reverse-slicing.js
+++ src/js/codewars-string-reverse-slicing.js
@@ -6,18 +6,19 @@
  * (a) in the reverse order of the original string, and
  * (b) with each successive string starting one character further in from the end of the original string.
  */
-function reverseSlice(str){
+module.exports = function reverseSlice(str){
     var data = str.split('').reverse().join(''),
         arr = [];
     for(var i = 0; i < str.length; i++){
         arr.push(data.slice(i, str.length));
     }
     return arr;
-}
+};
 
+/*
 var result = reverseSlice('123');
 console.log(`Should return: ['321', '21', '1']. Output is: ${result}`);
 
 var result1 = reverseSlice('abcde');
 console.log(`Should return:  ['edcba', 'dcba', 'cba', 'ba', 'a']. Output is: ${result1}`);
-
+*/
