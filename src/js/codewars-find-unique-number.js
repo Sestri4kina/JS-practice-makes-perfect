79fa38281bc6b19d26b01c080f3cb17b14248e10
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-17 13:05:22 +0200

    Add changes to the 'find unique number' to apply test

--- src/js/codewars-find-unique-number.js
+++ src/js/codewars-find-unique-number.js
@@ -4,12 +4,13 @@
  * There is an array with some numbers.
  * All numbers are equal except for one. Try to find it!
  */
-function findUniq(data) {
+module.exports = function findUniq(data) {
     return data.find((x, index, arr) => {
         return x !== arr[index + 1] && x !== arr[index - 1];
     });
-}
+};
 
+/*
 var result = findUniq([ 1, 1, 1, 2, 1, 1 ]);
 console.log(`Should return: 2. Output is: ${result}
 `);
@@ -17,5 +18,5 @@ console.log(`Should return: 2. Output is: ${result}
 var result1 = findUniq([ 0, 0, 0.55]);
 console.log(`Should return: 0.55. Output is: ${result1}
 `);
-
+*/
 
