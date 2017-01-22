87be99ca3310d13c15b9bcd875cc3f806e066960
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-22 22:35:16 +0200

    Add changes to the 'estimating distinct subsets' to apply test

--- src/js/codewars-estimating-distinct-subsets.js
+++ src/js/codewars-estimating-distinct-subsets.js
@@ -4,14 +4,15 @@
  * Given a set of elements (integers or string characters) that may occur more than once,
  * we need to know the amount of subsets that none of their values have repetitions.
  */
-function estSubsets(arr) {
+module.exports = function estSubsets(arr) {
     var data = [arr[0]];
     arr.forEach(x => {
         if(data.indexOf(x) === -1) data.push(x);
     });
     return arr.length === 0 ? 0 : (Math.pow(2, data.length) - 1);
-}
+};
 
+/*
 var result = estSubsets([1, 2, 3, 4]);
 console.log(`Should return: 15. Output is: ${result}
 `);
@@ -27,4 +28,5 @@ console.log(`Should return: 0. Output is: ${result2}
 var result3 = estSubsets([1, 1, 1, 1]);
 console.log(`Should return: 1. Output is: ${result3}
 `);
+*/
 
