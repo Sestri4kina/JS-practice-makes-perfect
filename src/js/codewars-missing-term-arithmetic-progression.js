5e1007b4fd2a5a95c2e83557b2ca52cf548e07b9
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-01-24 13:03:05 +0200

    Add changes to the 'missing term arithmetic progression' to apply test

--- src/js/codewars-missing-term-arithmetic-progression.js
+++ src/js/codewars-missing-term-arithmetic-progression.js
@@ -1,13 +1,16 @@
 /**
  * Created by Sestri4kina on 23.01.2017.
+ *
+ * Function returns a missing term in an arithmetic progression.
  */
-function findMissingTerm(list){
+module.exports = function findMissingTerm(list){
     var diff = (list[1] - list[0] === list[2] - list[1]) ? list[1] - list[0] :
         (Math.abs(list[1] - list[0]) > Math.abs(list[2] - list[1])) ? list[2] - list[1]
             : list[1] - list[0];
     return list.find((x, index, arr) => {return x + diff !== arr[index+1]}) + diff;
-}
+};
 
+/*
 var result = findMissingTerm([ -12, -17, -22, -27, -37, -42 ]);
 console.log(`Should return: -32. Output is: ${result}
 `);
@@ -15,3 +18,4 @@ console.log(`Should return: -32. Output is: ${result}
 var result1 = findMissingTerm([ 1, -5, -8 ]);
 console.log(`Should return: -2. Output is: ${result1}
 `);
+*/
