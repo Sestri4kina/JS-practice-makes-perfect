ff5bac5735ab6ed7c548e37546dd0d512d15b2c8
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-02-05 22:41:18 +0200

    Add solution for the 'vodka festival'

---
+++ src/js/codewars-vodka-festival.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Sestri4kina on 05.02.2017.
+ *
+ * https://www.codewars.com/kata/vodka-festival/javascript
+ * Write function which accepts an array of drank amounts from various cups and calculate
+ * total amount of vodka drunk by god.Result must be rounded to nearest one and
+ * can be return both in literes or mililitres.
+ */
+function vodkaConsumption(shots){
+    var capacity = shots
+        .map(x => {
+            if ( x.endsWith('ml') ) return +x.replace(/ml/, '');
+            else if ( x.endsWith('l') ) return +x.replace(/l/, '') * 1000;
+
+        })
+        .reduce((prev, curr) => { return prev + curr});
+
+    return `${capacity}ml`;
+}
+
+var result = vodkaConsumption(["40ml","5.5l"]);
+console.log(`Should return: '5540ml'. Output is: ${result}
+`);
+
+var result3 = vodkaConsumption(["500ml","2l","1.4l"]);
+console.log(`Should return: '3900ml'. Output is: ${result3}
+`);
+
