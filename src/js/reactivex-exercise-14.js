959bf773282d242cbae3f955fb379910e8d48566
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-02-07 13:36:45 +0200

    Add solution for the 'reactivex exercise 14'

---
+++ src/js/reactivex-exercise-14.js
@@ -0,0 +1,110 @@
+/**
+ * Created by Sestri4kina on 07.02.2017.
+ */
+Array.prototype.concatAll = function () {
+    var results = [];
+    this.forEach(function (subArray) {
+        if (Array.isArray(subArray)) results.push(...subArray);
+    });
+    return results;
+};
+
+Array.prototype.concatMap = function(projectionFunctionThatReturnsArray) {
+    return this.
+    map(function(item) {
+        return projectionFunctionThatReturnsArray(item);
+    }).concatAll();
+};
+
+/*
+ var spanishFrenchEnglishWords = [ ["cero","rien","zero"], ["uno","un","one"], ["dos","deux","two"] ];
+ // collect all the words for each number, in every language, in a single, flat list
+ var allWords = [0,1,2].
+    concatMap(function(index) {
+    return spanishFrenchEnglishWords[index];
+ });
+
+ return JSON.stringify(allWords) === '["cero","rien","zero","uno","un","one","dos","deux","two"]';
+ */
+
+function getIdTitleBoxart() {
+    var movieLists = [
+        {
+            name: "Instant Queue",
+            videos : [
+                {
+                    "id": 70111470,
+                    "title": "Die Hard",
+                    "boxarts": [
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 4.0,
+                    "bookmark": []
+                },
+                {
+                    "id": 654356453,
+                    "title": "Bad Boys",
+                    "boxarts": [
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" }
+
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 5.0,
+                    "bookmark": [{ id: 432534, time: 65876586 }]
+                }
+            ]
+        },
+        {
+            name: "New Releases",
+            videos: [
+                {
+                    "id": 65432445,
+                    "title": "The Chamber",
+                    "boxarts": [
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 4.0,
+                    "bookmark": []
+                },
+                {
+                    "id": 675465,
+                    "title": "Fracture",
+                    "boxarts": [
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+                        { width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 5.0,
+                    "bookmark": [{ id: 432534, time: 65876586 }]
+                }
+            ]
+        }
+    ];
+
+
+    // Use one or more concatMap, map, and filter calls to create an array with the following items
+    // [
+    //	 {"id": 675465, "title": "Fracture", "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+    //	 {"id": 65432445, "title": "The Chamber", "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+    //	 {"id": 654356453, "title": "Bad Boys", "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
+    //	 {"id": 70111470, "title": "Die Hard", "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+    // ];
+
+    return movieLists.concatMap(movie => movie.videos.map(video => {
+        return {"id": video.id,
+            "title": video.title,
+            "boxart": video.boxarts
+                .map(boxart => boxart.url)
+                .filter(url => url.endsWith("150.jpg"))
+                .toString()
+        };
+    }));
+}
+
+console.log(getIdTitleBoxart());
