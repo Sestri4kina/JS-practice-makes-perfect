9f11c3d5f60064d06aa865c27fd43bc33eb6bccd
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-02-17 23:54:24 +0200

    Add solution to the 'reverse and invert'

---
+++ src/js/codewars-reverse-and-invert.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Sestri4kina on 17.02.2017.
+ *
+ * https://www.codewars.com/kata/reverse-and-invert/javascript
+ */
+function numberReverseInvert(num){
+    let tmp = num < 0 ? -num : num,
+        sign = num < 0 ? 1 : -1;
+    return sign * parseFloat(tmp.toString().split('').reverse().join(''));
+}
+
+function reverseInvert(array){
+    return array.filter(x => { return (typeof x === 'number') && (x % 1 === 0) })
+        .map(x => { return numberReverseInvert(x) });
+}
+
+
+var result = reverseInvert([1,12,'a',3.4,87,99.9,-42,50,5.6]);
+console.log(`Should return: [-1,-21,-78,24,-5]. Output is: ${result}
+`);
+
+var result1 = reverseInvert([-9,-18,99]);
+console.log(`Should return: [9,81,-99]. Output is: ${result1}
+`);
