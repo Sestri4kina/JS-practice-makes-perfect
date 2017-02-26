eb4abb3b1e4fa73654cf7bd1cdf1ffb534dcd032
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-02-26 23:05:29 +0200

    Add changes to 'cut array into parts' to apply test

--- src/js/codewars-cut-array-into-parts.js
+++ src/js/codewars-cut-array-into-parts.js
@@ -3,15 +3,16 @@
  *
  * https://www.codewars.com/kata/cut-array-into-smaller-parts/javascript
  */
-function makeParts(arr, chunkSize) {
+module.exports = function makeParts(arr, chunkSize) {
     if (arr.length < chunkSize) return [arr];
     var result = [];
     for (var i = 0, limit = arr.length; i < limit; i += chunkSize){
         result.push(arr.slice(i, i + chunkSize));
     }
     return JSON.stringify(result);
-}
+};
 
+/*
 var result = makeParts([1,2,3,4,5], 2);
 console.log(`Should return: [[1,2],[3,4],[5]]. Output is: ${result}
 `);
@@ -23,4 +24,4 @@ console.log(`Should return: [[1],[2],[3]]. Output is: ${result3}
 var result1 = makeParts([1,2,3,4,5], 10);
 console.log(`Should return: [[1,2,3,4,5]]. Output is: ${result1}
 `);
-
+    */
