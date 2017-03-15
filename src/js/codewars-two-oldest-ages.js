c2ebe7db496888812a2e503cf6e774938a5bae1d
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-03-15 22:30:54 +0200

    Add solution for the 'two oldest ages'

---
+++ src/js/codewars-two-oldest-ages.js
@@ -0,0 +1,11 @@
+/*
+* created by Sestri4kina on 15.03.2017
+*
+* https://www.codewars.com/kata/two-oldest-ages-1/train/javascript
+*/
+function twoOldestAges(ages){
+    return ages.slice().sort((a,b) => {return a-b}).slice(-2);
+}
+
+let result = twoOldestAges( [1, 2, 10, 8] );
+console.log(`Should return: [8, 10]. Output is: ${result}`);
