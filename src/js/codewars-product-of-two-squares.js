76393cabeadb353a0ca3906a454ae260e958b2c1
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-03-31 17:48:58 +0300

    Add changes to apply test

--- src/js/codewars-product-of-two-squares.js
+++ src/js/codewars-product-of-two-squares.js
@@ -3,7 +3,7 @@
 *
 * https://www.codewars.com/kata/product-of-two-squares/train/javascript
 */
-function squareProduct(n) {
+module.exports = function squareProduct(n) {
   let arr = [],
     num = Math.sqrt(n),
     limit = Math.sqrt(num);
@@ -11,8 +11,9 @@ function squareProduct(n) {
     if (num % i === 0) arr.push([i, num/i]);
   }
   return arr;
-}
+};
 
+/*
 let result = squareProduct(16);
 console.log(`Should return: [[1,4],[2,2]]. Case#1: ${JSON.stringify(result)}`);
 
@@ -20,4 +21,5 @@ let result1 = squareProduct(256);
 console.log(`Should return: [[2,8],[4,4],[1,16]]. Case#2: ${JSON.stringify(result1)}`);
 
 let result2 = squareProduct(2);
-console.log(`Should return: []. Case#3: ${JSON.stringify(result2)}`);
+console.log(`Should return: []. Case#3: ${JSON.stringify(result2codewars)}`);
+*/
