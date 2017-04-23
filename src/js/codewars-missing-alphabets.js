48216ae6aa337955915ef8f9ecab0f29b3b17348
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-04-23 22:19:55 +0300

    Add solution for the 'missing alphabets'

---
+++ src/js/codewars-missing-alphabets.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Sestri4kina on 23.04.2017.
+ *
+ * https://www.codewars.com/kata/simple-fun-number-135-missing-alphabets/train/javascript
+ */
+ function missingAlphabets(s) {
+   let alphabet = 'abcdefghijklmnopqrstuvwxyz',
+     arr = [], limit = alphabet.length,
+     limit2 = s.length, maxOccur = 0,
+     result = '';
+   for (let i = 0; i < limit; i++) {
+     let el = alphabet.charAt(i);
+     arr.push([el, 0]);
+   }
+   for (let i = 0; i < limit2; i++) {
+     let ind = s.charCodeAt(i);
+     arr[ind - 97][1] += 1;
+     if (arr[ind - 97][1] > maxOccur) maxOccur = arr[ind - 97][1];
+   }
+   for (let i = 0; i < limit; i++) {
+     result += arr[i][0].repeat(maxOccur - arr[i][1]);
+   }
+   return result;
+ }
+
+ let result = missingAlphabets("codewars");
+ console.log(`Should return: "bfghijklmnpqtuvxyz". Output is: ${result}
+ `);
+
+ let result1 = missingAlphabets("abbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxy");
+ console.log(`Should return: "ayzz". Output is: ${result1}
+ `);
+
+ let result2 = missingAlphabets("abcdefghijklmnopqrstuvwxy");
+ console.log(`Should return: "z". Output is: ${result2}
+ `);
