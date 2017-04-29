59a12fa36e3d4e2205b5cdaff8018c1984cc5360
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-04-29 21:43:37 +0300

    Add changes to apply test

--- src/js/codewars-flatten-and-sort-array.js
+++ src/js/codewars-flatten-and-sort-array.js
@@ -3,14 +3,14 @@
  *
  * https://www.codewars.com/kata/flatten-and-sort-an-array/train/javascript
  */
- function flattenAndSort(arr) {
+ module.exports = function flattenAndSort(arr) {
    let newArr = [], limit = arr.length;
    for (let i = 0; i < limit; i++) {
      newArr = newArr.concat(arr[i]);
    }
    return newArr.sort((a,b) => a-b);
- }
-
+ };
+/*
  let result = flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]);
  console.log(`Should return: [1, 2, 3, 4, 5, 6, 7, 8, 9]. Output is: ${result}
  `);
@@ -18,3 +18,4 @@
  let result1 = flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]);
  console.log(`Should return: [1, 2, 3, 4, 5, 6, 100]. Output is: ${result1}
  `);
+*/
