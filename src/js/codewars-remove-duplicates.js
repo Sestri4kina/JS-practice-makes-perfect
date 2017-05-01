003b46772f7f0832d8c739cd3f57f1c1bda71f18
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-05-01 14:14:17 +0300

    Add solution for the 'remove duplicates'

---
+++ src/js/codewars-remove-duplicates.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Sestri4kina on 01.05.2017.
+ *
+ * https://www.codewars.com/kata/remove-duplication/train/javascript
+ */
+ function removeDuplication(arr){
+   let result = arr.filter((x, id, arr) => arr.indexOf(x) === arr.lastIndexOf(x));
+   return result;
+ }
+
+ let result = removeDuplication([1,2,3,2,4,6,2,6,7]);
+ console.log(`Should return: [1, 3, 4, 7]. Output is: ${result}
+ `);
+
+ let result1 = removeDuplication([1,2,1,2,1,1,2,2]);
+ console.log(`Should return: []. Output is: ${result1}
+ `);
