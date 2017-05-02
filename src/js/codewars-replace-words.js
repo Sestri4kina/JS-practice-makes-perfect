6a33e81708f6b57c32c7f63426e854536f888384
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-05-02 23:31:00 +0300

    Add changes to apply test

--- src/js/codewars-replace-words.js
+++ src/js/codewars-replace-words.js
@@ -3,7 +3,7 @@
  *
  * https://www.codewars.com/kata/simple-fun-number-218-replace-words/train/javascript
  */
-function replaceWords(sentence) {
+module.exports = function replaceWords(sentence) {
   let arr = sentence.split(' '),
     sortedArr = arr.slice()
     .sort((a, b) => {return a.length - b.length}),
@@ -18,9 +18,10 @@ function replaceWords(sentence) {
   result = result.map(x => x !== 'I' ? x.toLowerCase() : x);
   result[0] = result[0].charAt(0).toUpperCase() + result[0].slice(1);
 
-  return result.join(' '); 
-}
+  return result.join(' ');
+};
 
+/*
 let result = replaceWords("I am the best");
 console.log(`Should return: "Best the am I". Output is: ${result}
 `);
@@ -28,3 +29,4 @@ console.log(`Should return: "Best the am I". Output is: ${result}
 let result1 = replaceWords("I am better than him");
 console.log(`Should return: "Better than I am him". Output is: ${result1}
 `);
+*/
