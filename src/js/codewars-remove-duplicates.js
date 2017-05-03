bec97c359d9dbc2304ce0196346574837bd7ae83
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-05-03 22:57:23 +0300

    Add changes to apply test

--- src/js/codewars-remove-duplicates.js
+++ src/js/codewars-remove-duplicates.js
@@ -3,11 +3,12 @@
  *
  * https://www.codewars.com/kata/remove-duplication/train/javascript
  */
- function removeDuplication(arr){
+ module.exports = function removeDuplication(arr){
    let result = arr.filter((x, id, arr) => arr.indexOf(x) === arr.lastIndexOf(x));
    return result;
- }
+ };
 
+/*
  let result = removeDuplication([1,2,3,2,4,6,2,6,7]);
  console.log(`Should return: [1, 3, 4, 7]. Output is: ${result}
  `);
@@ -15,3 +16,4 @@
  let result1 = removeDuplication([1,2,1,2,1,1,2,2]);
  console.log(`Should return: []. Output is: ${result1}
  `);
+*/
